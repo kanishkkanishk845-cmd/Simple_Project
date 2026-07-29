cf8cff752584eb850a073f404e8b24417345bf97
R.kanishk <kanishkkanishk845@gmail.com>
2026-07-29 14:51:05 +0530

    Good morning

---
+++ kanishk.R
@@ -0,0 +1,10 @@
+[Running] python -u "c:\Users\lab1\Downloads\kanishk.R\sample.py"
+R.kanishk
+
+[Done] exited with code=0 in 0.031 seconds
+
+[Running] python -u "c:\Users\lab1\Downloads\kanishk.R\sample.py"
+R.kanishk
+
+[Done] exited with code=0 in 0.036 seconds
+
